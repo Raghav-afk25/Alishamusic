fbe6c5f6d94d6077aeed05bb2896b4799a43e8f8
Raghav-afk25 <raghavagarwalq12345@gmail.com>
2024-11-02 21:29:29 +0530

    Update live.py

--- AnonXMusic/plugins/play/live.py
+++ AnonXMusic/plugins/play/live.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 
-from AnonXMusic import YouTube, app
+from AnonXMusic import Spotify, app
 from AnonXMusic.utils.channelplay import get_channeplayCB
 from AnonXMusic.utils.decorators.language import languageCB
 from AnonXMusic.utils.stream.stream import stream
